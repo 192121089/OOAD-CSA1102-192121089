a84ec3970f78b43a244f93200dde659a136e813f
192121089 <110813928+192121089@users.noreply.github.com>
2023-08-16 10:49:24 +0530

    Add files via upload

---
+++ uml-generated-code/book bank managemnet system/authentication.java
@@ -0,0 +1,91 @@
+
+import java.util.*;
+
+
+/**
+ * Class authentication
+ */
+public class authentication {
+
+  //
+  // Fields
+  //
+
+  private void username;
+  private void password;
+  private void login;
+  
+  //
+  // Constructors
+  //
+  public authentication () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  /**
+   * Set the value of username
+   * @param newVar the new value of username
+   */
+  private void setUsername (void newVar) {
+    username = newVar;
+  }
+
+  /**
+   * Get the value of username
+   * @return the value of username
+   */
+  private void getUsername () {
+    return username;
+  }
+
+  /**
+   * Set the value of password
+   * @param newVar the new value of password
+   */
+  private void setPassword (void newVar) {
+    password = newVar;
+  }
+
+  /**
+   * Get the value of password
+   * @return the value of password
+   */
+  private void getPassword () {
+    return password;
+  }
+
+  /**
+   * Set the value of login
+   * @param newVar the new value of login
+   */
+  private void setLogin (void newVar) {
+    login = newVar;
+  }
+
+  /**
+   * Get the value of login
+   * @return the value of login
+   */
+  private void getLogin () {
+    return login;
+  }
+
+  //
+  // Other methods
+  //
+
+  /**
+   */
+  public void login()
+  {
+  }
+
+
+}
---
+++ uml-generated-code/passport automation system/passport_automation.java
@@ -0,0 +1,31 @@
+
+
+/**
+ * Class passport_automation
+ */
+public class passport_automation {
+
+  //
+  // Fields
+  //
+
+  
+  //
+  // Constructors
+  //
+  public passport_automation () { };
+  
+  //
+  // Methods
+  //
+
+
+  //
+  // Accessor methods
+  //
+
+  //
+  // Other methods
+  //
+
+}
